9da97f30ba00de641a93b174eea2f797f5408252
Alexander Kochkin <kochkina@btlnet.com>
2022-02-13 23:51:12 +0500

    Added automatic tests.

--- Program.cs
+++ Program.cs
@@ -7,29 +7,10 @@ namespace Power
     {
         static void Main(string[] args)
         {
-          double num = 1.001;
-          int power = 1000;
-          Stopwatch watch = new Stopwatch();
-          watch.Start();
-          double result = PowerTrivial(num, power);
-          watch.Stop();
-          string time = watch.Elapsed.TotalMilliseconds.ToString();
-          Console.WriteLine($"{num}^{power} using trivial algorithm takes {time} ms.");
-        }
-
-        static double PowerTrivial(double number, int pow)
-        {
-          double result = 1;
-          for(int i=0; i<pow; i++)
-          {
-            result *= number;
-          }
-          return result;
-        }
-
-        static double Power1(double number, int pow)
-        {
-          return 0;
+          ITask power = new Power();
+          Tester tester = new Tester(power, @".\tests\");
+          tester.RunAllTests();
+          // tester.RunTest(6);
         }
     }
 }
